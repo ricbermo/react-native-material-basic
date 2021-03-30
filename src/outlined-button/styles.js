ee08f2dda843f4955653085288647d9539d0156e
Ricardo Berdejo <ricardo@fullstacklabs.co>
2021-03-30 12:19:36 -0500

    Adjust outlined button width

--- src/outlined-button/styles.js
+++ src/outlined-button/styles.js
@@ -2,7 +2,7 @@ import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
   outlined: {
-    borderWidth: StyleSheet.hairlineWidth,
+    borderWidth: 1,
     borderColor: 'grey',
   },
 });
